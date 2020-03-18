5f8ad0b958fc3fc364b714686cf28aa948e19298
Andrei Mulev <a.mulev.vironit@vironit.ru>
2020-03-18 20:08:28 +0300

    Cleaned index.js

--- src/index.js
+++ src/index.js
@@ -17,10 +17,6 @@ io.sockets.on('connection', socket => {
 	socket.on('create', async room => {
 		socket.join(room);
 		console.log(`You are in room: ${room}`);
-		// const messages = await message_service.getAllMessagesByDialogId(room);
-		// socket.to(`${room}`).emit('add message', messages);
-		// console.log('Enter');
-		// console.log('Rooms', socket.adapter.rooms);
 	});
 
 	socket.on('send message in Room', data => {
@@ -38,21 +34,9 @@ io.sockets.on('connection', socket => {
 		}
 	});
 
-	// socket.on('get messages from Room', async room => {
-	// 	try {
-	// 		socket.join(room);
-	// 		const messages = await message_service.getAllMessagesByDialogId(room);
-	// 		socket.to(`${room}`).emit('add message from room', messages);
-	// 	} catch (error) {
-	// 		console.log('Error in index.js', error);
-	// 	}
-	// });
-
 	socket.on('leave', room => {
 		socket.leave(room);
 		console.log(`You are leaving room: ${room}`);
-		// console.log('Rooms', socket.adapter.rooms);
-		// console.log('Exit');
 	});
 
 	socket.on('disconnect', data => {
@@ -65,28 +49,9 @@ io.sockets.on('connection', socket => {
 			message: data.message,
 			name: data.name,
 			ownerId: data.ownerId,
-			// dialogId: 'chat-room',
 		});
-		// message_service.add(data);
 	});
 });
-
-// const chat = io
-// .of('/chat')
-// .on('connection', socket => {
-// 	socket.emit('a message', {
-// 		that: 'only'
-// 		'/chat':'will get'
-// 	});
-
-//events for client
-// client.on('subscribeToTimer', interval => {
-// 	console.log('client is subscribing to timer with interval ', interval);
-// 	setInterval(() => {
-// 		client.emit('timer', new Date());
-// 	}, interval);
-// });
-// });
 
 io.listen(port);
 console.log('listening on port ', port);
