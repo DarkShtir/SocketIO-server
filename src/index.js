c4f441cdd2e41588402e84b47d474a9ee90209eb
Andrew Muliou <andreyjedai@tut.by>
2020-04-28 15:16:18 +0300

    Fix path to env

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 const path = require('path');
-require('dotenv').config({ path: './config/dev.env' });
+require('dotenv').config({
+	path: path.join(__dirname + '/config/' + 'dev.env'),
+});
 const mongoose = require('mongoose');
 
 const MessageService = require('./services/message-service');
 const message_service = new MessageService();
 
 const io = require('socket.io')();
-const port = process.env.PORT || 8000;
+const port = process.env.SOCKET_PORT || 8000;
 
 const connection = [];
 
-io.sockets.on('connection', socket => {
+io.sockets.on('connection', (socket) => {
 	console.log('Succesfull connection');
 	connection.push(socket);
 
-	socket.on('create', async room => {
+	socket.on('create', async (room) => {
 		socket.join(room);
 		console.log(`You are in room: ${room}`);
 	});
 
-	socket.on('send message in Room', data => {
+	socket.on('send message in Room', (data) => {
 		try {
 			socket.join(data.dialogId);
 			io.sockets.to(data.dialogId).emit('add message in room', {
@@ -34,17 +36,17 @@ io.sockets.on('connection', socket => {
 		}
 	});
 
-	socket.on('leave', room => {
+	socket.on('leave', (room) => {
 		socket.leave(room);
 		console.log(`You are leaving room: ${room}`);
 	});
 
-	socket.on('disconnect', data => {
+	socket.on('disconnect', (data) => {
 		connection.splice(connection.indexOf(socket), 1);
 		console.log('Disconnect');
 	});
 
-	socket.on('send message', data => {
+	socket.on('send message', (data) => {
 		io.sockets.emit('add message', {
 			message: data.message,
 			name: data.name,
@@ -53,12 +55,12 @@ io.sockets.on('connection', socket => {
 	});
 });
 
-io.listen(port);
-console.log('listening on port ', port);
+// io.listen(port);
+// console.log('listening on port ', port);
 
 async function start() {
 	try {
-		await mongoose.connect(process.env.MONGO_DB, {
+		await mongoose.connect(process.env.SOCKET_MONGO_DB, {
 			useCreateIndex: true,
 			useNewUrlParser: true,
 			useFindAndModify: false,
